380f93bac5964a1a3394fb5b4f49de4593ef2660
Shweta Agrawal <ag.shweta88@gmail.com>
2016-11-21 21:27:25 +0100

    Method two

---
+++ method2.php
@@ -0,0 +1,39 @@
+<?php
+/*
+FileName: method2.php
+Createdby: Shweta Agrawal
+CreatedDate: 21/11/2016 16:30
+Purpose: OOPS based Method to print numbers
+*/
+
+// parent class
+class printNumbers {
+   
+   // constructor
+   public function __construct() {
+      $this->print0();
+   }
+	
+   public function __call($method, $_) {
+      $count = str_replace('print', '', $method);
+	   if($count % 3==0 && $count % 5==0){echo 'invivox';}
+	   else if($count % 3==0){echo 'invi';}
+	   else if($count % 5==0){echo 'vox';}
+	   else{echo $count;}
+	   echo '<br/>';
+	   
+      $this->{"print" . ++$count}();
+   }
+}
+
+// child class
+class printHundredNumbers extends printNumbers {
+   public function print101() {}
+}
+
+new printHundredNumbers;
+
+
+
+
+?>
