fe2f1f15b4bf4cb7227f0da356d7ae424ddd9ce6
Shweta Agrawal <ag.shweta88@gmail.com>
2016-11-22 12:24:48 +0100

    jquery files for method3.php

---
+++ invivox/jquery/lazyload.js
@@ -0,0 +1,44 @@
+var lazyload = lazyload || {};
+
+(function($, lazyload) {
+
+    "use strict";
+
+    var page = 2,
+        buttonId = "#button-more",
+        loadingId = "#loading-div",
+        container = "#data-container";
+
+    lazyload.load = function() {
+
+        var url = "./" + page + ".php";
+        $(buttonId).hide();
+        $(loadingId).show();
+
+        $.ajax({
+            url: url,
+            success: function(response) {
+                if (!response || response.trim() == "NONE") {
+                    $(buttonId).fadeOut();
+                    $(loadingId).text("No more entries to load!");
+                    return;
+                }
+                appendContests(response);
+            },
+            error: function(response) {
+                $(loadingId).text("Sorry, there was some error with the request. Please refresh the page.");
+            }
+        });
+    };
+
+    var appendContests = function(response) {
+        var id = $(buttonId);
+
+        $(buttonId).show();
+        $(loadingId).hide();
+
+        $(response).appendTo($(container));
+        page += 1;
+    };
+
+})(jQuery, lazyload);
